63e59884332921772def31f94e66d43f8be5c47b
EllioAce <elliott.elijah12@gmail.com>
2023-12-22 18:12:52 +0200

    Fixed up execution comparison timings

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,30 @@ fn main() {
     let base: u8 = 2;
     let number: i32 = 6;
     let converted_bits: [u8; BASE_BITS_LENGTH] = number_to_bit_list(number, base);
-    let now = Instant::now();
-    print_normal_divison(number, base);
-    let mut since_division: f32 = now.elapsed().as_secs_f32();
-    println!("Divided normally in {}s", since_division);
-    since_division = now.elapsed().as_secs_f32() - since_division;
-    print_base_divison(converted_bits, base);
-    since_division = now.elapsed().as_secs_f32() - since_division;
-    println!("Divided using base division in {}s", since_division);
-    println!("{}", now.elapsed().as_secs_f32());
+
+    let now_normal = Instant::now();
+    let normal_result = normal_divison(number, base);
+    let elapsed_normal = now_normal.elapsed();
+    println!("Divided normally in {}s", elapsed_normal.as_secs_f32());
+
+    let now_base = Instant::now();
+    let base_result = base_divison(converted_bits, base);
+    let elapsed_base = now_base.elapsed();
+    println!(
+        "Divided using base division in {}s",
+        elapsed_base.as_secs_f32()
+    );
+
+    println!("Normal division result: {}", normal_result);
+    println!("Base division result: {}", base_result);
 }
 
-fn print_base_divison(bits: [u8; BASE_BITS_LENGTH], denominator: u8) {
-    let division = divide_base_bit_number_array(bits, denominator);
-    println!("{}", division);
+fn base_divison(bits: [u8; BASE_BITS_LENGTH], denominator: u8) -> i32 {
+    let division: i32 = divide_base_bit_number_array(bits, denominator);
+    return division;
 }
 
-fn print_normal_divison(number: i32, denominator: u8) {
-    println!("{}", number / denominator as i32);
+fn normal_divison(number: i32, denominator: u8) -> i32 {
+    let result: i32 = number / denominator as i32;
+    return result;
 }
